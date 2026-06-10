eb5c299db1066a0e5649d042f8cb0117da8cbac3
Nagendra <gndevsecops@gmail.com>
2026-06-10 21:13:22 +0530

    varible

--- 07-special var.sh
+++ 07-special var.sh
@@ -1,3 +1,8 @@
 #!/bash/bin
 
 echo "all varible passed to script : $@"
+
+echo "number of varible password : $#"
+echo "first varible : $1"
+echo "script name :$0"
+echo "who is runnig this :$USER"
