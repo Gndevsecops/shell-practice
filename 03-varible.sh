5b93b087f89ef84b49063b944e4ab391e5ac6e47
Nagendra <gndevsecops@gmail.com>
2026-06-10 19:31:39 +0530

    varible

--- 03-varible.sh
+++ 03-varible.sh
@@ -3,7 +3,7 @@
 person1=ramu
 person2=seetha
 
-echo "$person1 : hey, $person1rara "
+echo "$person1 : hey, $person1 rara "
 echo "$person2 : hey come ra"
 echo "$person1 :pora $person2 yedava"
 echo "$person2 :namasthey $person1 bhey"
