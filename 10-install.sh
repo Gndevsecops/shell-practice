fce8bf9d4fa807f8ce6cb49dd41f2e2a6715cd24
Nagendra <gndevsecops@gmail.com>
2026-06-10 23:03:43 +0530

    test

--- 10-install.sh
+++ 10-install.sh
@@ -7,10 +7,12 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
    echo "please run this script with root access"
    exit 1
+fi       
 
-fi
- 
- echo " I am contunig ..."
+  echo " installing mysql"
+  dnf install mysql -y
+
+  
 
 
   
