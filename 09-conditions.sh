94f32c29cc6f0dcfbae5b92606085e163123e337
Nagendra <gndevsecops@gmail.com>
2026-06-10 22:22:56 +0530

    test

---
+++ 09-conditions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# gt  -greater than
+# lt  -Less than
+# eq  -Equal
+# ne  -not Equall
+# ge  -greater than or equal
+
+if[$NUMBER -ge 20]; then
+   echo "Give number $NUMBER is greater then to 20"
+
+elif[$NUMBER -eq 20]; then
+   echo  " Give number $NUMBER is equall to 20"
+
+else[$NUMBER -lt 20]; then
+  echo "give number $NUMBER is less than 20"
+
+fi 
